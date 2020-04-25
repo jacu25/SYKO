ec083ba8c4e492cbdc48920b48de96fbb1deac11
jacu25 <tel55@o2.pl>
2020-04-25 17:57:52 +0200

    Revert "Merge branch 'master' of https://github.com/jacu25/SYKO"
    This reverts commit dc3a3056175025f4fcfdc7d25ef10bc336969b59, reversing
    changes made to 37da09151b08045a97c23de29bc2a451ae9ea804.

--- IR.vhd
+++ IR.vhd
@@ -1,40 +1,13 @@
-library IEEE;
+ibrary IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity IR is
  
 	generic(
-		delay : time 
-	);
+		DELAY : time );
 	
 	port( 
 		ie, oe, clk, rst : in std_logic;
 		ir_in :in std_logic_vector(7 downto 0);
-		ir_out : out std_logic_vector(7 downto 0)
-	);
-		
-		
-end IR;
-
-architecture arch of PC is
-begin 
-	p1: 
-	process (clk, ie, oe, rst)
-
-	variable store : std_logic_vector (7 downto 0);
-	begin
-		if rising_edge(clk) then
-			if rst=’0’ then
-				store := “00000000”;
-			elsif ie=’1’ then
-				store := ir_in;
-			end if;
-		elsif falling_edge(clk) then
-			if oe='1' then
-				ir_out <= store after delay;
-			else
-				ir_ir <= “ZZZZZZZZ” after delay;
-			end if
-		end if;
-	end process;
-end arch;
+		ir_out : out std_logic_vector(7 downto 0));
+end
