a17b7776af18e3bf0cbd03dcc7c4821bd9109028
TytanAlfa <bhbo99@gmail.com>
2020-06-09 20:23:48 +0200

    IR

--- IR.vhd
+++ IR.vhd
@@ -17,19 +17,32 @@ entity IR is
 end IR;
 
 architecture arch of IR is
+signal r_e : std_logic := '0';
+
 begin 
-	 
-	process (clk, ie, rst)
+
+	clock: process (clk) is
+
+		begin
+			if rising_edge(clk) then
+				r_e<= '1';
+			elsif falling_edge(clk) then
+				r_e<= '0';
+			end if;
+	end process;
+	
+	
+	process (r_e, ie, rst)
 
 	variable store : std_logic_vector (ND downto 0);
 	begin
-		if rising_edge(clk) then
+		if r_e = '1' then
 			if rst='0' then
 				store := (others=>'0');
 			elsif ie='1' then
 				store := ir_in;
 			end if;
-		elsif falling_edge(clk) then
+		elsif r_e ='0' then
 			ir_out <= store after delay;
 		end if;
 	end process;
